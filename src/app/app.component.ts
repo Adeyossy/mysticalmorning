32c46ab0d3b4ddc5688b1876724035011f5a49f3
Adeyossy <adeyossy1@gmail.com>
2023-03-16 17:34:13 +0100

    fix: adjusted fonts on devices and background blur

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,4 +9,8 @@ export class AppComponent {
   products = [
     'sandwich', 'chocolate', 'jollof_rice', 'coffee', 'fries', 'sandwich_chocolate_meal', 'fried_rice', 'drink', 'sandwiches'
   ];
+
+  preorderMessage = 'Hello, I would like to order sandwich and coffee drink against tomorrow morning. I would like it delivered by 8am. My location is _______________';
+
+  subscribeMessage = 'Hello, I would like to subscribe to your breakfast service. I would like to have sandwich and coffee (insert any other dish on our menu) every morning at 8.00am from Monday to Friday. You can deliver to _____________ location.';
 }
