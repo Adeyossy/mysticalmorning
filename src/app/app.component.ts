a94369eb19c0aaae48020d95b79c4b0e16e00e11
Adeyossy <adeyossy1@gmail.com>
2023-03-16 14:41:46 +0100

    feat: improved homepage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'mysticalmorning';
+  products = [
+    'sandwich', 'chocolate', 'jollof_rice', 'coffee', 'fries', 'sandwich_chocolate_meal', 'fried_rice', 'drink', 'sandwiches'
+  ];
 }
