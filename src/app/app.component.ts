0821cb7e5925a5d4fe374f88f79cbfcc36ba6f1e
Mustapha Adeyosola <adeyossy1@gmail.com>
2023-10-07 20:36:18 +0100

    feat: privacy policy ready for launch

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,5 +7,14 @@ import { Product } from './models/product';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  isModalShown = false;
+  preorderMessage = 'Hello, I would like to order sandwich and coffee drink against tomorrow morning. I would like it delivered by 8am. My location is ________';
+
+  subscribeMessage = 'Hello, I would like to subscribe to your breakfast service. I would like to have sandwich and coffee (insert any other dish on our menu) every morning at 8.00am from Monday to Friday. You can deliver to _____________ location.';
+
+
+  toggleModal(): void {
+    this.isModalShown = !this.isModalShown;
+  }
   
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -17,14 +17,14 @@ export class HomeComponent implements OnInit {
   productSandwich: Product = {
     name: 'Sandwich',
     category: 'Food',
-    price: 399,
+    price: 500,
     description: ''
   }
 
   productChocolate: Product = {
     name: 'Chocolate',
     category: 'Drink',
-    price: 399,
+    price: 500,
     description: ''
   }
 
@@ -38,10 +38,6 @@ export class HomeComponent implements OnInit {
   affordableProducts = [this.productSandwich, this.productChocolate, this.productMeal]
 
   more_products = ['fries'];
-
-  preorderMessage = 'Hello, I would like to order sandwich and coffee drink against tomorrow morning. I would like it delivered by 8am. My location is ________';
-
-  subscribeMessage = 'Hello, I would like to subscribe to your breakfast service. I would like to have sandwich and coffee (insert any other dish on our menu) every morning at 8.00am from Monday to Friday. You can deliver to _____________ location.';
   
   isModalShown = false;
 
