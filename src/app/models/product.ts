082c79d5c22b2b121bf7bce71e95d6c78b763f3d
Mustapha Adeyosola <adeyossy1@gmail.com>
2024-03-18 11:23:12 +0100

    feat: Add Review interface and update Product interface

--- src/app/models/product.ts
+++ src/app/models/product.ts
@@ -1,7 +1,25 @@
+export interface Review {
+    userId: string;
+    summary: string;
+    description: string;
+    overallRating: number;
+    productId: string;
+    foodQualityRating: number;
+    deliveryRating: number;
+    customerServiceRating: number;
+    valueForMoneyRating: number;
+    willRecommend: number;  // should translate from 100%, 75%, 50%, 25%, 0%
+}
+
 export interface Product {
-    // productId: string;
+    productId: string;
     name: string;
     category: string;
     price: number;
     description: string;
+    nutritionalValue: string;
+    rating: number;
+    reviews: string; // concat string Ids into a single string to save space
+    images: string; // concat the links of each image into a single string
+    lifestyleImages: string;
 }
