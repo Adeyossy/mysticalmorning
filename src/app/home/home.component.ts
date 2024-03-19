2af3282ac512a93347959a2329e0a4bf70826af9
Mustapha Adeyosola <adeyossy1@gmail.com>
2024-03-19 04:30:34 +0100

    feat: Set images property on Product instances

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -8,22 +8,26 @@ import { AppService } from '../services/app.service';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-  constructor(public appService: AppService) {}
-
-  ngOnInit() {
-  }
-  
   products = [
     'sandwich', 'product_spaghetti', 'spaghetti.in.pan', 'coffee', 'product_rice', 'sandwiches'
   ];
-
+  
   productFries = new Product("", "Potatoes and Eggs", 700, "Food");
   productSpaghetti = new Product("", "Spaghetti Stirfry", 800, "Food");
   productRice = new Product("", "Mystical Jollof Rice", 2500, "Food");
-
+  
   affordableProducts = [this.productFries, this.productSpaghetti, this.productRice]
-
+  
   more_products = ['product_fries'];
-
+  
   stars = new Array(5);
+  
+  constructor(public appService: AppService) {
+    this.productFries.images = "../../../assets/images/product_fries.jpg";
+    this.productSpaghetti.images = "../../../assets/images/product_spaghetti.jpg";
+    this.productRice.images = "../../../assets/images/product_rice.jpg";
+  }
+  
+  ngOnInit() {
+  }
 }
