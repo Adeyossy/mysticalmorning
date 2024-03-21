308d06a92bc346997cf5b105134d211eea1d2392
Mustapha Adeyosola <adeyossy1@gmail.com>
2024-03-21 20:42:15 +0100

    feat: Get products from firestore database

--- src/app/services/app.service.ts
+++ src/app/services/app.service.ts
@@ -9,9 +9,31 @@ export class AppService {
   isModalShown = false;
   products: Product[] = [];
 
-  constructor(private firebaservice: FirebaseService) { }
+  constructor(private firebaservice: FirebaseService) {
+    this.fetchProducts();
+  }
 
   toggleModal(): void {
     this.isModalShown = !this.isModalShown;
+  }
+
+  fetchProducts() {
+    const fetchSub = this.firebaservice.getCollection$<Product>("products").subscribe({
+      next: (value) => {
+        this.products = value;
+        fetchSub.unsubscribe();
+      },
+      error: (err) => {
+        console.log(err);
+      },
+      complete: () => {
+        fetchSub.unsubscribe();
+      }
+    })
+  }
+
+  getProducts() {
+    if (this.products.length === 0) {}
+    return this.products;
   }
 }
