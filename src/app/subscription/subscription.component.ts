1d191a244ce1be6aa9dacb9b058e61f44e15a85e
Mustapha Adeyosola <adeyossy1@gmail.com>
2024-04-10 19:01:04 +0100

    feat: Generate and configure subscription component with steps

---
+++ src/app/subscription/subscription.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-subscription',
+  templateUrl: './subscription.component.html',
+  styleUrls: ['./subscription.component.css']
+})
+export class SubscriptionComponent {
+  steps = [
+    {
+      heading: "Choose the days of the week you want your food delivered",
+      body: `You can order breakfast, lunch and/or dinner for each day of the week and Mystical 
+      Morning will automagically deliver it to you on those days.`
+    },
+    {
+      heading: "Choose the food you want for each day",
+      body: `You can choose from any of our mystical meals for your breakfast, 
+      lunch and/or dinner for each day of the week. You may be able to change your meal if you 
+      want something else that morning, afternoon or evening.`
+    },
+    {
+      heading: "Indicate where and when you want your food delivered",
+      body: `e.g. I would like my breakfast delivered by 8:00am in UCH, Ibadan. You will be able 
+      to send a message to Mystical Morning's WhatsApp line once you complete your subscription.`
+    }
+  ]
+}
