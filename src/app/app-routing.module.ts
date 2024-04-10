2ff35a40d3a273597e9ee39c13e34ed72ec5bee8
Mustapha Adeyosola <adeyossy1@gmail.com>
2024-04-10 20:34:59 +0100

    feat: Add new subscribe route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,13 @@ import { AppComponent } from './app.component';
 import { PrivacyComponent } from './minicomponents/privacy/privacy.component';
 import { HomeComponent } from './home/home.component';
 import { ArticleComponent } from './article/article.component';
+import { SubscriptionComponent } from './subscription/subscription.component';
 
 const routes: Routes = [
   { path: "", component: HomeComponent },
   { path: "about/privacy", component: PrivacyComponent },
-  { path: "articles/balanced-diet-and-the-five-food-groups", component: ArticleComponent }
+  { path: "articles/balanced-diet-and-the-five-food-groups", component: ArticleComponent },
+  { path: "subscribe", component: SubscriptionComponent, title: "Subscribe to a Food Plan" }
 ];
 
 @NgModule({
