3b1eac30a86311da102b09cfa4a4106e84cc9a41
Mustapha Adeyosola <adeyossy1@gmail.com>
2024-04-12 15:51:07 +0100

    feat: Create new routes for subscription

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,12 +5,18 @@ import { PrivacyComponent } from './minicomponents/privacy/privacy.component';
 import { HomeComponent } from './home/home.component';
 import { ArticleComponent } from './article/article.component';
 import { SubscriptionComponent } from './subscription/subscription.component';
+import { NewSubscriptionComponent } from './subscription/new-subscription/new-subscription.component';
 
 const routes: Routes = [
   { path: "", component: HomeComponent },
   { path: "about/privacy", component: PrivacyComponent },
   { path: "articles/balanced-diet-and-the-five-food-groups", component: ArticleComponent },
-  { path: "subscribe", component: SubscriptionComponent, title: "Subscribe to a Food Plan" }
+  { path: "subscribe", component: SubscriptionComponent, 
+    title: "Subscribe to Mystical Morning's Food Plan" },
+  { path: "subscribe/new/steps/:stepNumber/:stepDescription", component: NewSubscriptionComponent, 
+    title: "New Mystical Morning Food Plan", children: [
+      // { path: "placeholder" }
+    ] }
 ];
 
 @NgModule({
