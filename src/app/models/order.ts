536d3400d153cbec3b3e556b979c6d07505ee818
Mustapha Adeyosola <adeyossy1@gmail.com>
2024-04-30 20:48:44 +0100

    feat: Add BasicOrder model

--- src/app/models/order.ts
+++ src/app/models/order.ts
@@ -1,12 +1,19 @@
-export type Order = {
+import { Product } from "./product";
+
+export const ORDER_COLLECTION = "orders";
+
+export type TimeOfMeal = "Breakfast" | "Lunch" | "Dinner";
+
+export interface Order {
   userId: string;
   orderId: string;
   items: string[];
   orderDate: number;
-  timeOfMeal: "breakfast" | "lunch" | "dinner";
+  timeOfMeal: "Breakfast" | "Lunch" | "Dinner";
   deliveryDate: number;
   deliveryLocation: string;
-  deliveryContact: string;
+  deliveryContact: string; // can be used as a unique identifier
+  deliveryBackup: string;
   price: number;
   paymentMethod: "cash" | "transfer" | "pos" | "card";
   isConfirmed: boolean;
@@ -14,4 +21,35 @@ export type Order = {
   isCancelled: boolean;
   isNeglected: boolean;
   subscriptionId: string;
+}
+
+export class Order {
+  userId = "";
+  orderId = "";
+  items: string[] = [];
+  orderDate = 0;
+  timeOfMeal: "Breakfast" | "Lunch" | "Dinner" = "Breakfast";
+  deliveryDate = 0;
+  deliveryLocation = "";
+  deliveryContact = ""; // can be used as a unique identifier
+  deliveryBackup = "";
+  price = 0;
+  paymentMethod: "cash" | "transfer" | "pos" | "card" = "transfer";
+  isConfirmed = false;
+  isDelivered = false;
+  isCancelled = false;
+  isNeglected = false;
+  subscriptionId = "";
+}
+
+export type BasicOrder = {
+  timeOfMeal: "Breakfast" | "Lunch" | "Dinner";
+  items: Product[];
+  price: number;
+}
+
+export type BasicOrderSummary = {
+  timeOfMeal: "Breakfast" | "Lunch" | "Dinner";
+  items: string;
+  price: number;
 }
